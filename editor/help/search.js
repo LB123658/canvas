e1fc2c32b51fcbc9904f3845484b85673d8ab563
lb123658 <79328251+LB123658@users.noreply.github.com>
2023-02-04 14:56:38 -0600

    Update search.js

--- editor/help/search.js
+++ editor/help/search.js
@@ -2,6 +2,9 @@ var sidebar = document.getElementById("sidebar");
 var content = document.getElementById("content");
 var q = decodeURIComponent(location.search.split("=")[1].replace(/\+/gi, " "));
 var none = `<h2>No results matched your search</h2><p>Return to the Editor Help homepage for all topics.</p><a href="index.html" target="_blank">Help homepage</a><br><a href="../index.html" target="_blank">Open Editor <span class="material-icons" style="font-size: 14px">launch</span></a>`;
+var inventory = 0;
+
+content.innerHTML = "";
 
 function loadResults(x) {
   if (x.toLowerCase().indexOf(q.toLowerCase()) != -1) {
@@ -9,13 +12,14 @@ function loadResults(x) {
     var link = document.createElement("p");
     link.innerHTML = "<a href='index.html?topic=" + number + "'>" + question[number] + "</a><br><div style='width:50%; white-space:nowrap; overflow:hidden; text-overflow:ellipsis; color:black; text-decoration:none;'>" + directions[number] + "</div><br>";
     content.appendChild(link);
-  } else {
-    content.innerHTML = none;
-  }
+    inventory++;
+  } 
 }
-
-content.innerHTML = "";
 
 for (let i = 0; i < directions.length ;i++) {
     loadResults(directions[i]);
+}
+
+if (inventory === 0) {
+    content.innerHTML = none;
 }
