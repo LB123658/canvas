a667c994f1fe30c4d4cd1cab3922ee9fd77252f2
lb123658 <79328251+LB123658@users.noreply.github.com>
2023-02-04 14:49:14 -0600

    Update search.js

--- editor/help/search.js
+++ editor/help/search.js
@@ -1,6 +1,6 @@
 var sidebar = document.getElementById("sidebar");
 var content = document.getElementById("content");
-var q = decodeURIComponent(location.search.split("=")[1].replace(/\+/gi, " ")).toLowerCase();
+var q = decodeURIComponent(location.search.split("=")[1].replace(/\+/gi, " "));
 var none = `<h2>No results matched your search</h2><p>Return to the Editor Help homepage for all topics.</p><a href="index.html" target="_blank">Help homepage</a><br><a href="../index.html" target="_blank">Open Editor <span class="material-icons" style="font-size: 14px">launch</span></a>`;
 
 function loadResults(x) {
@@ -13,6 +13,8 @@ function loadResults(x) {
     content.innerHTML = none;
   }
 }
+
+content.innerHTML = "";
 
 for (let i = 0; i < directions.length ;i++) {
     loadResults(directions[i]);
