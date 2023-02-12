12e8f4710532443cb98df04526894dee420927e4
lb123658 <79328251+LB123658@users.noreply.github.com>
2023-02-11 20:21:46 -0600

    Update search.js

--- editor/help/search.js
+++ editor/help/search.js
@@ -1,5 +1,6 @@
 var sidebar = document.getElementById("sidebar");
 var content = document.getElementById("content");
+var searchBar = document.getElementById("search");
 var q = decodeURIComponent(location.search.split("=")[1].replace(/\+/gi, " "));
 var none = `<h2>No results matched your search</h2><p>Return to the Editor Help homepage for all topics.</p><a href="index.html" target="_blank">Help homepage</a><br><a href="../index.html" target="_blank">Open Editor <span class="material-icons" style="font-size: 14px">launch</span></a>`;
 var inventory = 0;
@@ -23,3 +24,6 @@ for (let i = 0; i < directions.length ;i++) {
 if (inventory === 0) {
     content.innerHTML = none;
 }
+
+//show search in search bar
+searchBar.value = q;
